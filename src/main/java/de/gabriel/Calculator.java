5e4175c5e4098a78f1a4cf05b1832baf6953e028
Gabriel Taubert <gabrieltaubert03@gmail.com>
2026-01-02 16:40:48 +0100

    create first class with test

---
+++ src/main/java/de/gabriel/Calculator.java
@@ -0,0 +1,20 @@
+package de.gabriel;
+
+public class Calculator {
+
+    public double add (double a, double b) {
+        return a + b;
+    }
+
+    public double sub (double a, double b) {
+        return a - b;
+    }
+
+    public double mult (double a, double b) {
+        return a * b;
+    }
+
+    public double div (double a, double b) {
+        return a / b;
+    }
+}
---
+++ src/test/java/de/gabriel/TestCalculator.java
@@ -0,0 +1,45 @@
+package de.gabriel;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestCalculator {
+
+    private Calculator calculator;
+
+    @BeforeEach
+    void setUp() {
+        calculator = new Calculator();
+    }
+
+    @Test
+    void test_add_function() {
+        double result = calculator.add(2,3);
+
+        assertEquals(5, result);
+    }
+
+    @Test
+    void test_sub_function() {
+        double result = calculator.sub(10,3);
+
+        assertEquals(7, result);
+    }
+
+    @Test
+    void test_mult_function() {
+        double result = calculator.mult(2,3);
+
+        assertEquals(6, result);
+    }
+
+    @Test
+    void test_div_function() {
+        double result = calculator.div(6,2);
+
+        assertEquals(3, result);
+    }
+
+}
